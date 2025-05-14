1fb8e3e4ad8d87ee7db84aaa9e1fad75d5ddfee0
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-14 18:03:04 -0400

    Created Course model. Added relation with User model

---
+++ src/main/java/com/coursesregister/courses/models/Course.java
@@ -0,0 +1,67 @@
+
+package com.coursesregister.courses.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name="courses")
+public class Course {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    private String title;
+    private String description;
+
+    @ManyToOne
+    private User createdBy;
+
+    public Course() {
+    }
+
+    public Course(UUID id, String title, String description, User createdBy) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdBy = createdBy;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+       
+}
