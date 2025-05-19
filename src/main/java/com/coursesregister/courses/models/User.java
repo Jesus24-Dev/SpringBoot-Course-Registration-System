0ed94305382cdd898af8c47032362d7a572236be
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-19 15:33:53 -0400

    Implements userDetails in user model. Added lazy fetch and added override methods

--- src/main/java/com/coursesregister/courses/models/User.java
+++ src/main/java/com/coursesregister/courses/models/User.java
@@ -6,35 +6,43 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
     
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     
-    @Column(unique=true)
+    @Column(unique = true, nullable = false)
     private String username;
+    
+    @Column(nullable = false)
     private String password;
     
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Roles roles;
     
-    @OneToMany(mappedBy = "createdBy")
-    private List<Course> coursesCreated = new ArrayList<>();
-    
-    @OneToMany(mappedBy = "user")
-    private List<Enrollment> usersEnrollments = new ArrayList<>();
+    @OneToMany(mappedBy = "createdBy", fetch = FetchType.LAZY)
+    private List<Course> coursesCreated;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Enrollment> usersEnrollments;
 
     public User() {
     }
@@ -54,6 +62,7 @@ public class User {
         this.id = id;
     }
 
+    @Override
     public String getUsername() {
         return username;
     }
@@ -62,6 +71,7 @@ public class User {
         this.username = username;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
@@ -92,6 +102,31 @@ public class User {
 
     public void setUsersEnrollments(List<Enrollment> usersEnrollments) {
         this.usersEnrollments = usersEnrollments;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(roles.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
       
 }
