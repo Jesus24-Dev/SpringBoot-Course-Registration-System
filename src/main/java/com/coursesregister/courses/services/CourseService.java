d9124c700dbb74516d30dfdf12502e489f8ec0f5
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-20 15:39:24 -0400

    Implements CRUD operations for courses

--- src/main/java/com/coursesregister/courses/services/CourseService.java
+++ src/main/java/com/coursesregister/courses/services/CourseService.java
@@ -2,19 +2,24 @@
 package com.coursesregister.courses.services;
 
 import com.coursesregister.courses.models.Course;
+import com.coursesregister.courses.models.User;
 import com.coursesregister.courses.repository.CourseRepository;
+import com.coursesregister.courses.repository.UserRepository;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final UserRepository userRepository;
     
     @Autowired
-    public CourseService(CourseRepository courseRepository){
+    public CourseService(CourseRepository courseRepository, UserRepository userRepository){
         this.courseRepository = courseRepository;
+        this.userRepository = userRepository;
     }
     
     public List<Course> getAllCourses(){
@@ -25,5 +30,39 @@ public class CourseService {
         Course course = courseRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Course not found. Id: " + id));
         return course;
+    }
+    
+    public List<Course> getCoursesByAdmin(String username){
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found. Username: " + username));
+        
+        return user.getCoursesCreated();
+    }
+    
+    public void createCourse(String title, String description, String username){
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found. Username: " + username));
+        
+        Course courseCreated = new Course();
+        courseCreated.setTitle(title);
+        courseCreated.setDescription(description);
+        courseCreated.setCreatedBy(user);
+        
+        courseRepository.save(courseCreated);
+    }
+    
+    public void updateCourseDescription(String description, UUID courseId){
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new RuntimeException("Course not found. Id: " + courseId));
+        
+        course.setDescription(description);
+        courseRepository.save(course);
+    }
+    
+    public void deleteCourse(UUID courseId){
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new RuntimeException("Course not found. Id: " + courseId));
+        
+        courseRepository.delete(course);
     }
 }
