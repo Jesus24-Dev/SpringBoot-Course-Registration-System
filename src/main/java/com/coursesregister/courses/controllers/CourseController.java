f925fe428cdd9c409778e0bf4bf56e2643a83f8f
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-20 14:54:41 -0400

    Add controllers for courses and courses detail page

--- src/main/java/com/coursesregister/courses/controllers/CourseController.java
+++ src/main/java/com/coursesregister/courses/controllers/CourseController.java
@@ -1,14 +1,43 @@
 
 package com.coursesregister.courses.controllers;
 
+import com.coursesregister.courses.models.Course;
+import com.coursesregister.courses.services.CourseService;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CourseController {
+          
+    
+    private final CourseService courseService;
+    
+    @Autowired
+    public CourseController(CourseService courseService){
+        this.courseService = courseService;
+    }
     
     @GetMapping("/home")
     public String homePage(){
-        return "/home";
+        return "home";
+    }
+    
+    @GetMapping("/courses")
+    public String coursesPage(Model model){
+        List<Course> courses = courseService.getAllCourses();
+        model.addAttribute("courses", courses);
+        return "courses";
+    }
+    
+    @GetMapping("/courses/{id}")
+    public String courseDetail(Model model, @PathVariable UUID id){
+        Course course = courseService.getCourseById(id);
+        model.addAttribute("course", course);
+        return "course-detail";
     }
 }
