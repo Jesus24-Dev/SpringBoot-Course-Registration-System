aa0b4f5bade6186e47e54cdcecc81059024dda94
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-20 15:36:43 -0400

    Created CRUD operations for admin in Course entity

---
+++ src/main/java/com/coursesregister/courses/controllers/AdminCourseController.java
@@ -0,0 +1,61 @@
+package com.coursesregister.courses.controllers;
+
+import com.coursesregister.courses.dtos.CourseRequest;
+import com.coursesregister.courses.models.Course;
+import com.coursesregister.courses.services.CourseService;
+import jakarta.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminCourseController {
+    
+    private final CourseService courseService;
+    
+    @Autowired
+    public AdminCourseController(CourseService courseService){
+        this.courseService = courseService;
+    }
+    
+    @GetMapping("/courses")
+    public String getAdminCourses(Model model, Principal principal){
+        List<Course> coursesList = courseService.getCoursesByAdmin(principal.getName());
+        model.addAttribute("courses", coursesList);
+        
+        return "courses";  
+    }
+    
+    @PostMapping("/create")
+    public String createCourse(@Valid @ModelAttribute("course") CourseRequest course, BindingResult result){
+        if(result.hasErrors()){
+            return "register";
+        }
+        
+        courseService.createCourse(course.getTitle(), course.getDescription(), course.getUsername());     
+        return "courses?created=true";
+    }
+    
+    @PostMapping("/create/{id}")
+    public String updateCourseDescription(@PathVariable UUID id, String description){
+        courseService.updateCourseDescription(description, id);
+        
+        return "courses?updated=true";
+    }
+    
+    @PostMapping("/delete/{id}")
+    public String deleteCourse(@PathVariable UUID id){
+        courseService.deleteCourse(id);
+        return "courses?deleted=true";
+    }
+}
